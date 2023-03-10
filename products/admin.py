7bf509efdcb015eb7559411b10d91aa523a1926e
Anatoly <seruderu@gmail.com>
2023-03-10 14:58:00 +0300

    added admin page, product model changed

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Product, Category
 
-# Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'size', 'quantity', 'price', 'category', 'active', 'created']
+    search_fields = ['title', 'size', 'category__title', 'active']
+
+admin.site.register(Product, ProductAdmin)
+
+admin.site.register(Category)
+
--- products/models.py
+++ products/models.py
@@ -1,8 +1,12 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 
 class Category(models.Model):
     title = models.CharField(max_length=150)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+        ordering = ['title']
 
     def __str__(self):
         return self.title
@@ -10,11 +14,36 @@ class Category(models.Model):
 
 
 class Product(models.Model):
+
+    EXTRA_SMALL = 'XS'
+    SMALL = 'S'
+    MEDIUM = 'M'
+    LARGE = 'L'
+    EXTRA_LARGE = 'XL'
+
+    SIZE_CHOICES = [
+        (EXTRA_SMALL, 'XS'),
+        (SMALL, 'S'),
+        (MEDIUM, 'M'),
+        (LARGE, 'L'),
+        (EXTRA_LARGE, 'XL')
+    ]
+
+
+
     title = models.CharField(max_length=150, unique=True)
     price = models.DecimalField(max_digits=5, decimal_places=2)
-    desc = models.TextField()
-    image = models.CharField(max_length=200)
-    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    desc = models.TextField(null=True, blank=True)
+    image = models.CharField(null=True, blank=True, max_length=200)
+    quantity = models.PositiveBigIntegerField(default=0)
+    active = models.BooleanField(default=False)
+    category = models.ForeignKey('Category', related_name='products', on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(User, related_name='products', null=True, on_delete=models.CASCADE)
+    size = models.CharField(max_length=50, choices=SIZE_CHOICES, default=MEDIUM)
+
+    class Meta:
+        ordering = ['title']
 
     def __str__(self):
         return self.title

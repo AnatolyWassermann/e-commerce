e14c04fddb683d430ed354b7ac002556ba775283
Anatoly <seruderu@gmail.com>
2023-03-30 21:56:36 +0300

    added django filters and redis caching for products and category list views

--- rest_api/filters.py
+++ rest_api/filters.py
@@ -1,15 +1,30 @@
 from django_filters import rest_framework as filters
-from products.models import Product
+from products.models import Product, Category
+from django_filters.widgets import BooleanWidget
+
 
 class ProductFilter(filters.FilterSet):
     title = filters.CharFilter(lookup_expr='icontains')
-    price = filters.NumberFilter(lookup_expr='icontains')
-    quantity = filters.NumberFilter(lookup_expr='icontains')
-    active = filters.BooleanFilter(lookup_expr='icontains')
-    created = filters.DateFilter()
+    price = filters.RangeFilter(field_name='price')
+    quantity = filters.RangeFilter(field_name='quantity')
+    active = filters.BooleanFilter(widget=BooleanWidget())
+    created = filters.DateFromToRangeFilter(
+        label='Created Between',
+        label_suffix = ' (YYYY-MM-DD)',
+        help_text='Filter products created within a date range'
+    )
     size = filters.ChoiceFilter(choices=Product.SIZE_CHOICES)
 
     class Meta:
         model = Product
         fields = ['title', 'price', 'quantity', 'active', 'size', 'created']
+
+
+
+class CategoryFilter(filters.FilterSet):
+    title = filters.CharFilter(lookup_expr='icontains')
+
+    class Meta:
+        model = Category
+        fields = ['title']
 
--- rest_api/views.py
+++ rest_api/views.py
@@ -1,8 +1,12 @@
 from products.models import Product, Category
 from .serializers import ProductSerializer, CategorySerializer
 from rest_framework import viewsets, generics
-from .filters import ProductFilter
+from .filters import ProductFilter, CategoryFilter
 from django_filters import rest_framework as filters
+from django.views.decorators.cache import cache_page
+from django.utils.decorators import method_decorator
+from django.views.decorators.vary import vary_on_cookie
+
 
 
 class ProductViewSet(viewsets.ModelViewSet):
@@ -10,12 +14,23 @@ class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     filter_backends = (filters.DjangoFilterBackend,)
-    filter_class = ProductFilter
+    filterset_class = ProductFilter
+    @method_decorator(vary_on_cookie)
+    @method_decorator(cache_page(60*15))
+    def dispatch(self, *args, **kwargs):
+        return super(ProductViewSet, self).dispatch(*args, **kwargs)
+
 
 class CategoryViewSet(viewsets.ModelViewSet):
 
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    filter_backends = (filters.DjangoFilterBackend,)
+    filterset_class = CategoryFilter
+    @method_decorator(vary_on_cookie)
+    @method_decorator(cache_page(60*15))
+    def dispatch(self, *args, **kwargs):
+        return super(CategoryViewSet, self).dispatch(*args, **kwargs)
 
 class ProductDetailApiView(generics.RetrieveUpdateDestroyAPIView):
     

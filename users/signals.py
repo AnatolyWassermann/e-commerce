8e7102a22021852bedd2dd5c82cc88c801bf1d91
Anatoly <seruderu@gmail.com>
2023-05-31 20:03:09 +0300

    removed unnecessary files

--- users/signals.py
+++ users/signals.py
@@ -5,6 +5,7 @@ from .models import User
 
 @receiver(post_save, sender=User)
 def send_new_account_email(sender, instance, created, **kwargs):
+    """ when a new user account created, send an e mail notification automatically"""
     if created:
         subject = 'Welcome to my E-Commerce API Project'
         message = 'Thank you for creating an account'
